fad2f3370469e4b310e60e69a8ca873fce55d30c
wschoi8640 <cws911112@gmail.com>
2019-04-29 17:27:03 +0900

    ADD original files

---
+++ src/hufs/cws/network/EchoServer.java
@@ -0,0 +1,31 @@
+package hufs.cws.network;
+
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+
+public class EchoServer 
+{
+	    public static void main(String[] args)
+	    {
+		    	ServerSocket server;
+		    	Socket sock;
+		    	List<ObjectOutputStream> listWriters = new ArrayList<ObjectOutputStream>();
+		    	
+		        try
+		        {
+		               server = new ServerSocket(10001);
+		               
+		               while(true)
+		               {
+		            	       sock = server.accept();
+				               new EchoServerThread(sock, listWriters).start();
+				       }
+		        } 
+		        catch(IOException e)
+		        {
+		               System.out.println(e);
+		        }
+	    }
+}
